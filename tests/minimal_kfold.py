1cbbb8502f4a14e05c582b67c31001d030e8123e
Hychiro <hierohenrique@gmail.com>
2025-04-29 10:45:32 -0300

    kfold issue resolved

--- tests/minimal_kfold.py
+++ tests/minimal_kfold.py
@@ -3,7 +3,7 @@ project_directory = os.getcwd()
 sys.path.append(project_directory)
 
 from bciflow.datasets.CBCIC import cbcic
-from bciflow.modules.core.kfold import kfold, windowing, apply_pre_folding
+from bciflow.modules.core.kfold import kfold
 from bciflow.modules.tf.bandpass.chebyshevII import chebyshevII
 from bciflow.modules.fe import logpower
 from bciflow.modules.analysis.metric_functions import accuracy
@@ -16,13 +16,10 @@ d3 = cbcic(subject = 3)
 
 
 pre_folding = {'tf': (chebyshevII, {})}
-pos_folding = {'tl': (ea(), {'source': [d2]}),
+pos_folding = {'tl': (ea(), {'source': [d1]}),
                'fe': (logpower, {'flating': True}),
                'clf': (lda(), {})}
 start_window=d1['events']['cue'][0]+0.5
-
-d2 = windowing(target=d2, start_test_window=[start_window])
-d2 = apply_pre_folding(target_dict=d2, start_test_window=[start_window], pre_folding=pre_folding)
 
 results = kfold(target=d1, 
                 start_window=start_window, 
